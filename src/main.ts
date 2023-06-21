c93e23ea5e80569cbba3ebec8cf51c8c28e21e94
kozlowskimateusz <mateusz.kozlowski@nextapps.co>
2023-06-21 09:57:39 +0200

    added generating openapi file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  fs.writeFileSync(
+    './postman/schemas/documentation.json',
+    JSON.stringify(document),
+  );
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
